ab26f97f0fd14c64e642d9fe7ac7fb3050cb2b35
Nick Quaranto <nick@quaran.to>
2018-01-08 12:09:51 -0500

    Go back to spans so marks can be inline again

--- src/plugins/Mark.js
+++ src/plugins/Mark.js
@@ -20,7 +20,7 @@ export const MarkPlugin = ({ hotkeys }) => ({
         return `mark_${mark.type}`
       }).join(' ')
 
-      return <p className={classNames} {...attributes}>{children}</p>
+      return <span className={classNames} {...attributes}>{children}</span>
     }
     return null
   },
