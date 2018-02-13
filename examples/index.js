3801d82603bdd9a33c20ad762626fb25e26ffecd
Ben Ogle <ogle.ben@gmail.com>
2018-02-12 16:52:36 -0800

    Clean up example index

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,10 @@
-const Chatterslate = require("../dist")
-const React = require("react")
-const ReactDOM = require("react-dom")
+const Chatterslate = require('../dist')
+const React = require('react')
+const ReactDOM = require('react-dom')
 
-const jsonString = "{\"kind\":\"value\",\"document\":{\"kind\":\"document\",\"data\":{},\"nodes\":[{\"kind\":\"block\",\"type\":\"paragraph\",\"isVoid\":false,\"data\":{},\"nodes\":[{\"kind\":\"text\",\"leaves\":[{\"kind\":\"leaf\",\"text\":\"hi\",\"marks\":[{\"kind\":\"mark\",\"type\":\"bold\",\"data\":{}}]}]}]}]}}"
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  const props = { ref: (editor) => { global.editor = editor }, initialValue: JSON.parse(jsonString) }
-  const element = document.getElementById("example")
+document.addEventListener('DOMContentLoaded', event => {
+  const props = { ref: editor => { global.editor = editor }, initialValue }
+  const element = document.getElementById('example')
   const editor = React.createElement(Chatterslate.TopicEditor, props)
   ReactDOM.render(editor, element)
 
@@ -22,3 +20,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
     window.location.reload()
   }
 })
+
+const initialValue = {
+  kind: 'value',
+  document: {
+    kind: 'document',
+    data: {},
+    nodes: [{
+      kind: 'block', type: 'paragraph', isVoid: false, data: {}, nodes: [{ kind: 'text', leaves: [{ kind: 'leaf', text: 'hi', marks: [{ kind: 'mark', type: 'bold', data: {} }] }] }],
+    }],
+  },
+}
