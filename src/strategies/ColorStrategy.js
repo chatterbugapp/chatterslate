b2c2abb92d002a8fd120ae4391ea774be12530dd
Nick Quaranto <nick@quaran.to>
2018-03-13 10:45:54 -0400

    Add a new keyboard shortcut to make it easier to apply the last color

--- src/strategies/ColorStrategy.js
+++ src/strategies/ColorStrategy.js
@@ -1,11 +1,12 @@
-const DEFAULT_COLOR = 'black'
+const DefaultColor = 'black'
 const hasAnyColor = value => (value.marks.some(mark => mark.type === 'color'))
+let lastColor = DefaultColor
 
 /**
  * When clicking a color block, if the selection has a color in it, remove it.
- * Otherwise, add a new color!
+ * Otherwise, add a new color! Fallback to lastColor used if none was set.
  */
-const ColorStrategy = (value, color) => {
+const ColorStrategy = (value, color = lastColor) => {
   const change = value.change()
 
   // Adapted from https://github.com/nossas/slate-editor/blob/master/lib/plugins/slate-color-plugin/ColorUtils.js
@@ -14,13 +15,12 @@ const ColorStrategy = (value, color) => {
       value.marks.filter(mark => mark.type === 'color').forEach(mark => {
         change.removeMark(mark)
       })
-
-      if (color !== DEFAULT_COLOR) {
-        change.addMark({ type: 'color', data: { color } }).focus()
-      }
     }
-  } else if (color !== DEFAULT_COLOR) {
+  }
+
+  if (color !== DefaultColor) {
     change.addMark({ type: 'color', data: { color } }).focus()
+    lastColor = color
   }
 
   return change
